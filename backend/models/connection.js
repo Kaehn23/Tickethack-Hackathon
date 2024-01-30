c584d9501c7c4b9b9cacb4fd848ac0a5256b6b43
Jeremy Ipa <jeremy.iparraguirre@gmail.com>
2024-01-30 13:11:33 +0100

    add name connection.js

--- backend/models/connection.js
+++ backend/models/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = 'mongodb+srv://admin:nvVEXUVLBYDjeStX@tugce.t5dgoyw.mongodb.net/'
+const connectionString = 'mongodb+srv://admin:nvVEXUVLBYDjeStX@tugce.t5dgoyw.mongodb.net/tickethack'
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
  .then(() => console.log('Database connected'))
