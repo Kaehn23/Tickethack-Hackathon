604066a9c8147a82ec4c309b434bf9912ab97d55
tugce <tugce.degentile@gmail.com>
2024-01-31 11:31:50 +0100

    save

--- backend/routes/cart.js
+++ backend/routes/cart.js
@@ -1,14 +1,18 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const Cart = require("../models/cart");
 
-router.get("/cart", function (req, res) {
+router.post("/", (req, res) => {
+  const { departure, arrival, date, price } = req.body; //
 
+  const newCart = new Cart({
+    departure: departure,
+    arrival: arrival,
+    date: date,
+    price: price,
+  });
 
-
-
-    
-})
-
-
+  newCart.save().then(() => res.json({ result: true, newCart }));
+});
 
 module.exports = router;
